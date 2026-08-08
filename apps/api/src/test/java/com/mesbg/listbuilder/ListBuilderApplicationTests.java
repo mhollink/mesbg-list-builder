fc2e811ca75d0cab330e49912da204064a2d6a9d
mhollink <hollink.mh@gmail.com>
2026-08-08 21:23:32 +0200

    chore: Init new project + scaffolding

---
+++ apps/api/src/test/java/com/mesbg/listbuilder/ListBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesbg.listbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
