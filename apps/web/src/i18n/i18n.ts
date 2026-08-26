b93ea89bdea55d064f418c93c23eb784a6bd8521
mhollink <hollink.mh@gmail.com>
2026-08-26 16:03:40 +0200

    Make sure only required language files are loaded

--- apps/web/src/i18n/i18n.ts
+++ apps/web/src/i18n/i18n.ts
@@ -1,17 +1,17 @@
 import i18n from "i18next";
+import resourcesToBackend from "i18next-resources-to-backend";
 import { initReactI18next } from "react-i18next";
-
-import commonEn from "./locales/en/common.json";
-import commonNl from "./locales/nl/common.json";
-
-import navigationEn from "./locales/en/navigation.json";
-import navigationNl from "./locales/nl/navigation.json";
-
-import settingsEn from "./locales/en/settings.json";
-import settingsNl from "./locales/nl/settings.json";
 
 void i18n
     .use(initReactI18next)
+    .use(
+        resourcesToBackend(
+            (language: string, namespace: string) =>
+                import(
+                    `./locales/${language}/${namespace}.json`
+                    ),
+        ),
+    )
     .init({
         lng: "en",
         fallbackLng: "en",
@@ -19,26 +19,6 @@ void i18n
         supportedLngs: [
             "en",
             "nl",
-        ],
-
-        resources: {
-            en: {
-                common: commonEn,
-                navigation: navigationEn,
-                settings: settingsEn,
-            },
-
-            nl: {
-                common: commonNl,
-                navigation: navigationNl,
-                settings: settingsNl,
-            },
-        },
-
-        ns: [
-            "common",
-            "navigation",
-            "settings",
         ],
 
         defaultNS: "common",
